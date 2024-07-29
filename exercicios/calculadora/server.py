d35d2632e19990c0b98372be7149801da788044b
liliabakker <liliabakker.lb@gmail.com>
2024-07-29 17:10:07 -0300

    feature: modified server.py

--- exercicios/calculadora/server.py
+++ exercicios/calculadora/server.py
@@ -1,24 +1,29 @@
 from Maths.mathematics import summation, subtraction, multiplication
+from flask import Flask, render_template, request
 
-app = FLask("Mathematics Problem Solver")
+app = Flask("Mathematics Problem Solver")
 
-app.route("/sum")
+@app.route("/sum")
 def sum_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    result = summation(num1, num2)
-    return str(result)
 
-app.route("/sub")
+
+@app.route("/sub")
 def sub_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    result = subtraction(num1, num2)
-    return str(result)
 
-app.route("/nul")
-def nul_route():
+
+@app.route("/mul")
+def mul_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    result = multiplication(num1, num2)
-    return str(result)
+
+
+@app.route("/")
+def render_index_page():
+
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
