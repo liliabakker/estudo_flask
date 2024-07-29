785614e05c6f096898cfa56090606d3800b3e2ca
liliabakker <liliabakker.lb@gmail.com>
2024-07-29 17:06:06 -0300

    feature: add index.html

---
+++ exercicios/calculadora/server.py
@@ -0,0 +1,24 @@
+from Maths.mathematics import summation, subtraction, multiplication
+
+app = FLask("Mathematics Problem Solver")
+
+app.route("/sum")
+def sum_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    result = summation(num1, num2)
+    return str(result)
+
+app.route("/sub")
+def sub_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    result = subtraction(num1, num2)
+    return str(result)
+
+app.route("/nul")
+def nul_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    result = multiplication(num1, num2)
+    return str(result)
